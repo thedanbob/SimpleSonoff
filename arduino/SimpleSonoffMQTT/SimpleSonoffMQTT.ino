35f31d7b4167e3134580d5700057d955b15eaa7b
Dan Arnfield <dan@arnfield.com>
2019-12-30 09:05:10 -0600

    Update previous maintainer's copyright

--- arduino/SimpleSonoffMQTT/SimpleSonoffMQTT.ino
+++ arduino/SimpleSonoffMQTT/SimpleSonoffMQTT.ino
@@ -1,6 +1,6 @@
 /*
   Copyright (c) 2019 Dan Arnfield
-  Copyright (c) 2017 @KmanOz
+  Copyright (c) 2017-2018 @KmanOz
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
